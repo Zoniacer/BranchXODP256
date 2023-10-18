341d95f48713a5ed765e646d7e83437f52948c4c
Zoniacer <63639703+Zoniacer@users.noreply.github.com>
2023-10-18 13:27:19 +0700

    CRUD data nasabah, data kunjungan, report kunjungan
    Kurang report tunggakan dan data SDB

--- face_recognition_app.py
+++ face_recognition_app.py
@@ -2,6 +2,7 @@ import face_recognition
 import cv2
 import numpy as np
 import os
+import csv
 
 # Initialize some variables
 face_locations = []
@@ -14,6 +15,7 @@ return_name='Unknown'
 
 for image in os.listdir('faces'):
     face_image = face_recognition.load_image_file(f'faces/{image}')
+    print(image)
     face_encoding = face_recognition.face_encodings(face_image)[0]
 
     known_face_encodings.append(face_encoding)
@@ -22,7 +24,7 @@ print(known_face_names)
 
 def process_frame(frame):
     global process_this_frame, known_face_encodings, known_face_names,face_names,return_name
-
+    nik=''
     # Only process every other frame of video to save time
     if process_this_frame:
         # Resize frame of video to 1/4 size for faster face recognition processing
@@ -50,13 +52,26 @@ def process_frame(frame):
             face_names.append(name)
 
     process_this_frame = not process_this_frame
-
+    display_name=''
     # Display the results
     for name in face_names:
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         cv2.rectangle(frame, (230, 420), (420, 370), (255, 255, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, (250, 400), font, 1.0, (0, 0, 0), 1)
-        return_name=name
+        with open('form_data.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)  # Create a CSV reader
 
-    return frame, return_name
+            # Specify the value you want to find
+            nik = name.replace(".jpg", "")
+            
+            # Loop through each row in the CSV file
+            for row in reader:
+                name2 = row['NIK']
+                if name2 == nik:
+                    display_name=row['Nama']
+                    
+                    
+        cv2.putText(frame, display_name.split(' ', 1)[0], (250, 400), font, 1.0, (0, 0, 0), 1)
+        return_name=display_name
+
+    return frame, return_name,nik
--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import cv2
 import face_recognition_app
 import os
 import base64
+import shutil
 import json
+import datetime
+import csv
 
-#TODO create data just using csv is fine for registration, sdb movie, and access
+#TODO create data just using csv is fine for sdb movie, and access
 
 app = Flask(__name__)
 
@@ -13,10 +16,17 @@ app.config['SERVER_NAME'] = '127.0.0.1:5000'  # Update with your specific server
 
 detection_complete = False
 client_name=''
+nik=''
 image_count = 0
 
 UPLOAD_FOLDER = 'temp'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def save_to_csv(data,csv_name):
+    with open(csv_name+'.csv', mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(data)
+
 
 @app.route('/capture', methods=['POST'])
 def capture():
@@ -39,7 +49,7 @@ def capture():
             img_bytes = base64.b64decode(image_data)
 
             # Save the image to a file
-            image_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'captured_image.png')
+            image_filename = os.path.join(app.config['UPLOAD_FOLDER'], 'captured_image.jpg')
             with open(image_filename, 'wb') as img_file:
                 img_file.write(img_bytes)
 
@@ -48,15 +58,15 @@ def capture():
     return 'No image data received'
 
 def generate_frames():
-    global detection_complete,client_name
+    global detection_complete,client_name,nik
     cap = cv2.VideoCapture(0)
     while True:
         success, frame = cap.read()
         if not success:
             break
         
-        frame,name = face_recognition_app.process_frame(frame)
-        print(name)
+        frame,name,nik = face_recognition_app.process_frame(frame)
+        # print(name)
         if name!='Unknown':
             detection_complete=True
             client_name=name.rsplit('.', 1)[0]
@@ -75,12 +85,28 @@ def video_feed():
 @app.route('/')
 def index():
     global detection_complete
-    if detection_complete:
-        return render_template('redirect_page.html')
     return render_template('index.html',detectionComplete=detection_complete)
     
 @app.route('/exit_video_feed')
 def exit_video_feed():
+    save_row=[]
+    global nik
+    date=datetime.datetime.now()
+    with open('form_data.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)  # Create a CSV reader
+
+        # Specify the value you want to find
+        target_name = nik
+        print(nik)
+            # Loop through each row in the CSV file
+        for row in reader:
+            name2 = row['NIK']
+            # print(row)
+            if name2 == target_name:
+                save_row=row
+    data=[date]
+    data.extend(list(save_row.values()))
+    save_to_csv(data,'kunjungan')
     return redirect(url_for('redirect_page'))
     
 @app.route('/redirect_page')
@@ -100,6 +126,30 @@ def check_detection():
 @app.route('/registration')
 def registration():
     return render_template('user_form.html')
+
+@app.route('/submit_user', methods=['POST'])
+def submit():
+    nama = request.form['nama']
+    rekening = request.form['rekening']
+    alamat = request.form['alamat']
+    email = request.form['email']
+    nomor_hp = request.form['nomor_hp']
+    nik = request.form['nik']
+    data=[nama,rekening,alamat,email,nomor_hp,nik]
+    save_to_csv(data,'form_data')
+    # Process the data here
+    
+    source_file = 'C:/Users/Hanif/Documents/Portofolio/Face Recognition/BranchXODP256/temp/captured_image.jpg'
+    destination_directory = 'C:/Users/Hanif/Documents/Portofolio/Face Recognition/BranchXODP256/faces/'
+
+    new_file_name = nik+'.jpg'
+
+    # Combine the destination directory and the new name to create the destination path
+    destination_path = f'{destination_directory}{new_file_name}'
+
+    # Move and rename the file
+    shutil.move(source_file, destination_path)
+    return 'Form data has been saved to CSV.'
 
 if __name__ == '__main__':
     app.run(debug=True)
